378f8c4537c3566841852c1c53fcf07434cad532
CamiFerraro <camiferraroo@gmail.com>
2023-01-11 19:43:54 -0300

    changes

--- DesarrolloApps_Ferraro/App.js
+++ DesarrolloApps_Ferraro/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import PlaceNavigator from "./navigation/PlaceNavigator"
 import { Provider } from "react-redux";
 import store from "../DesarrolloApps_Ferraro/store"
-import { init } from "./db";
+import {init} from "./db"
 
-init ()
-.then(()=> console.log("Database initialized")
-.catch((err)=>{
-  console.log("Database fail connect")
-  console.log(err.message)
-}))
+init()
+  .then(() => console.log("Database initialized"))
+  .catch((err) => {
+    console.log("Data base fail connect");
+    console.log(err.message);
+  });
 
 export default function App() {
   return (
     <Provider store={store}>
-      <PlaceNavigator/>
+      <PlaceNavigator />
     </Provider>
   );
 }
